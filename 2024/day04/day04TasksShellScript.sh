07e6cd7a51d1535409e8d077a941c8aada2729b3
deepakDevOp <deepak.513770@gmail.com>
2024-01-30 23:49:32 +0530

    Added day04 solutions

---
+++ 2024/day04/day04TasksShellScript.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Task01: Printing on console 'I will complete #90DaysOofDevOps challenge.'
+echo "I will complete #90DaysOofDevOps challenge."
+
+# Task02: Shell Script to take user input, input from arguments and print the variables.
+read -p "Enter your name: " name
+echo "$name"
+
+# taking arguments from command-line
+# enter two digits
+argument1=$1
+argument2=$2
+
+echo "$argument1, $argument2"
+
+# Task03: If else in Shell Scripting by comparing 2 numbers
+if [ "$argument1" -gt "$argument2" ]; then
+  echo "$argument1 is greater than $argument2"
+else
+  echo "$argument1 is less than $argument2"
+fi
